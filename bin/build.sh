0a621a3deb9e9bf4025ee4059ba6519df9ed051b
Jakub Igla <jakub.igla@gmail.com>
2020-05-21 18:26:03 +0100

    Unhardcode GA code

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
-set -euo pipefail
+set -e
 
 rm -rf dist && mkdir -p dist
 
 cp -rf src/* dist/
 envsubst < src/index.html > dist/index.html
+envsubst < src/404.html > dist/404.html
